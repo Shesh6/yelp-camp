cd33d67d35f5c2123b6f4e8eb9034858b9b7dd64
Shesh6 <rbananatwister@gmail.com>
2017-07-25 18:56:49 +0000

    add mLab

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     authRoutes       = require("./routes");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://shesh:Shesh6@ds123193.mlab.com:23193/yelpcamp_shesh");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
